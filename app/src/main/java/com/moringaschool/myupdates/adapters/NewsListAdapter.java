8b24924057bb5bf2a3177928c9990df7209f6de2
luciahroyalty101 <luciahkei101@gmail.com>
2021-02-03 18:52:21 +0300

    add an activity for details of the news

--- app/src/main/java/com/moringaschool/myupdates/adapters/NewsListAdapter.java
+++ app/src/main/java/com/moringaschool/myupdates/adapters/NewsListAdapter.java
@@ -1,34 +1,96 @@
 package com.moringaschool.myupdates.adapters;
 
+import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.moringaschool.myupdates.R;
 import com.moringaschool.myupdates.models.Article;
+import com.moringaschool.myupdates.ui.NewsDetailActivity;
 import com.moringaschool.myupdates.ui.NewsListActivity;
+import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
-public class NewsListAdapter extends RecyclerView.Adapter {
-    public NewsListAdapter(NewsListActivity newsListActivity, List<Article> top_headlines) {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class NewsListAdapter extends RecyclerView.Adapter<NewsListAdapter.NewsViewHolder> {
+    private List<Article> mTop_headlines;
+    private Context mContext;
 
+    public NewsListAdapter(Context context, List<Article> top_headlines) {
+        mContext = context;
+        mTop_headlines = top_headlines;
     }
 
-    @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+    public NewsListAdapter.NewsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.ativity_newsupdate_list, parent, false);
+        NewsViewHolder viewHolder = new NewsViewHolder(view);
+        return viewHolder;
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mTop_headlines.size();
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NewsListAdapter.NewsViewHolder holder, int position) {
+        holder.bindTop_headlines(mTop_headlines.get(position));
+    }
+
+    public class NewsViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        @BindView(R.id.NewsImageView)
+        ImageView mNewsImageView;
+        @BindView(R.id.TitleNameTextView)
+        TextView mTitleNameTextView;
+        //        @BindView(R.id.categoryTextView) TextView mCategoryTextView;
+        @BindView(R.id.authorTextView) TextView mAuthorTextView;
+//        @BindView(R.id.publishedAtTextView) TextView mPublishedAtTextView;
+
+        private Context mContext;
+
+        public NewsViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            mContext = itemView.getContext();
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            flipIt(v);
+            int itemPosition = getLayoutPosition();
+            Intent intent = new Intent(mContext, NewsDetailActivity.class);
+            intent.putExtra("position", itemPosition);
+            intent.putExtra("top_headlines", Parcels.wrap(mTop_headlines));
+            mContext.startActivity(intent);
+        }
+
+        private void flipIt(final View viewToFlip) {
+            ObjectAnimator flip = ObjectAnimator.ofFloat(viewToFlip, "rotationX", 0f, 360f);
+            flip.setDuration(200);
+            flip.start();
+        }
+
+        public void bindTop_headlines(Article top_headlines) {
+            mTitleNameTextView.setText(top_headlines.getTitle());
+            Picasso.get().load(top_headlines.getUrlToImage()).into(mNewsImageView);
+//            mCategoryTextView.setText(top_headlines.getContent());
+            mAuthorTextView.setText(top_headlines.getAuthor());
+//            mPublishedAtTextView.setText(top_headlines.getPublishedAt());
+        }
     }
 }
