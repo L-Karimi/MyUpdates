eb2439a38fdd4aa016ddab08018458b0ca9a0613
luciahroyalty101 <luciahkei101@gmail.com>
2021-02-03 19:26:11 +0300

    create article adapter to extend firebaserecycleradapter

---
+++ app/src/main/java/com/moringaschool/myupdates/adapters/articleAdapter.java
@@ -0,0 +1,97 @@
+package com.moringaschool.myupdates.adapters;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.moringaschool.myupdates.R;
+import com.moringaschool.myupdates.models.Article;
+import com.moringaschool.myupdates.util.Constants;
+import com.squareup.picasso.Picasso;
+
+public class articleAdapter extends FirebaseRecyclerAdapter<Article, articleAdapter.personsViewholder> {
+
+    private static final String TAG = "Bookmarked news";
+    DatabaseReference reference;
+    Context mContext;
+    private Article article;
+
+
+
+    public articleAdapter(@NonNull FirebaseRecyclerOptions<Article> options) {
+        super(options);
+    }
+
+    @NonNull
+    @Override
+    public personsViewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.news_list_item_drag, parent, false);
+        return new personsViewholder(view);
+
+
+    }
+
+
+    @Override
+    public void
+    onBindViewHolder(@NonNull personsViewholder holder, int position, @NonNull Article model) {
+
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = user.getUid();
+        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_TOP_HEADLINES).child(uid);
+        DatabaseReference usersRef = rootRef.child("top_headlines");
+        usersRef.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot ds : snapshot.getChildren()) {
+                    Article users = ds.getValue(Article.class);
+                    Log.d("result", "User name: " + users.getTitle() + ", email " + users.getDescription());
+                    Picasso.get().load(users.getUrlToImage()).into(holder.NewsImageView);
+                    holder.TitleNameTextView.setText(users.getTitle());
+                    holder.authorTextView.setText(users.getAuthor());
+
+
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+    }
+
+
+    class personsViewholder extends RecyclerView.ViewHolder {
+        public TextView TitleNameTextView, authorTextView;
+        public ImageView NewsImageView;
+        public personsViewholder(@NonNull View itemView)
+        {
+            super(itemView);
+
+            TitleNameTextView = itemView.findViewById(R.id.TitleNameTextView);
+            authorTextView = itemView.findViewById(R.id.authorTextView);
+            NewsImageView = itemView.findViewById(R.id.NewsImageView);
+        }
+    }
+}
+
--- app/src/main/java/com/moringaschool/myupdates/ui/BookmarkedNewsListActivity.java
+++ app/src/main/java/com/moringaschool/myupdates/ui/BookmarkedNewsListActivity.java
@@ -1,16 +1,62 @@
 package com.moringaschool.myupdates.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.moringaschool.myupdates.R;
+import com.moringaschool.myupdates.adapters.articleAdapter;
+import com.moringaschool.myupdates.models.Article;
+import com.moringaschool.myupdates.util.Constants;
 
 public class BookmarkedNewsListActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    articleAdapter adapter; // Create Object of the Adapter class
+    DatabaseReference mbase; // Create object of the
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bookmarked_list);
+        setContentView(R.layout.activity_master);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = user.getUid();
+
+
+        mbase = FirebaseDatabase.getInstance()
+                .getReference(Constants.FIREBASE_CHILD_TOP_HEADLINES)
+                .child(uid);
+
+        recyclerView = findViewById(R.id.recycler1);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        FirebaseRecyclerOptions<Article> options = new FirebaseRecyclerOptions.Builder<Article>()
+                .setQuery(mbase, Article.class)
+                .build();
+
+        adapter = new articleAdapter(options);
+        recyclerView.setAdapter(adapter);
+    }
+
+
+    @Override protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
     }
 }
+
